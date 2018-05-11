9098dd4ca3c1c9c820c4b32e99d3559bc783c86b
shenyang <shenyang@zto.cn>
2018-05-11 13:26:21 +0800

    'sy'

--- src/main/java/cn/no7player/config/Http2HttpsConfig.java
+++ src/main/java/cn/no7player/config/Http2HttpsConfig.java
@@ -15,30 +15,30 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Http2HttpsConfig {
 
-    @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
-        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory(){//1
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(httpConnector());
-        return tomcat;
-    }
-
-
-    @Bean
-    public Connector httpConnector(){//2
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setPort(8888);
-        connector.setSecure(false);
-        connector.setRedirectPort(9999);
-        return connector;
-    }
+//    @Bean
+//    public EmbeddedServletContainerFactory servletContainer() {
+//        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory(){//1
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint securityConstraint = new SecurityConstraint();
+//                securityConstraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection collection = new SecurityCollection();
+//                collection.addPattern("/*");
+//                securityConstraint.addCollection(collection);
+//                context.addConstraint(securityConstraint);
+//            }
+//        };
+//        tomcat.addAdditionalTomcatConnectors(httpConnector());
+//        return tomcat;
+//    }
+//
+//
+//    @Bean
+//    public Connector httpConnector(){//2
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setScheme("http");
+//        connector.setPort(8888);
+//        connector.setSecure(false);
+//        connector.setRedirectPort(9999);
+//        return connector;
+//    }
 }
